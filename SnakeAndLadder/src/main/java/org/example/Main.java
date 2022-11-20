9407bf2683f4a9b0056e4f4003eb212ed034c76d
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-20 16:51:34 +0530

    Added game played with single player at start position 0

---
+++ SnakeAndLadder/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int startPosition=0;
+        System.out.println("Start Position : "+startPosition);
+    }
+}
