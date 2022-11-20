0c7be1e1c395a3771a2b6c67f2a756916fc130df
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-21 03:39:30 +0530

    Added checks for a Option code

--- SnakeAndLadder/src/main/java/org/example/Main.java
+++ SnakeAndLadder/src/main/java/org/example/Main.java
@@ -1,10 +1,26 @@
 package org.example;
 
 public class Main {
+    public static final int initialPosition = 0;
+
     public static void main(String[] args) {
-        int startPosition=0;
-        System.out.println("Start Position : "+startPosition);
-        int playerDie=(int) Math.floor(Math.random() * 6 + 1);
-        System.out.println(playerDie); 
+        int currentPosition = initialPosition;
+        System.out.println("Start Position : " + initialPosition);
+        int playerDie = (int) Math.floor(Math.random() * 6 + 1);
+        int optionDie = (int) Math.floor(Math.random() * 3);
+        System.out.println(optionDie);
+        switch (optionDie) {
+            case 1:
+                System.out.println("Ladder");
+                currentPosition = currentPosition + playerDie;
+                break;
+            case 2:
+                System.out.println("Snake");
+                currentPosition = currentPosition - playerDie;
+                break;
+            default:
+                break;
+        }
+        System.out.println("Current place:- "+currentPosition);
     }
 }
