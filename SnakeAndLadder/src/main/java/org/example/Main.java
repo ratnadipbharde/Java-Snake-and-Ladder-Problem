5ada4d098d4538bfce4b171a11e30ec7078f4440
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-20 17:25:40 +0530

    Added roll the die to get a number between 1 to 6 solution

--- SnakeAndLadder/src/main/java/org/example/Main.java
+++ SnakeAndLadder/src/main/java/org/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int startPosition=0;
         System.out.println("Start Position : "+startPosition);
+        int playerDie=(int) Math.floor(Math.random() * 6 + 1);
+        System.out.println(playerDie);
+        
     }
 }
